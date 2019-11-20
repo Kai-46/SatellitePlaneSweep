dd8029c4039e4162c4e3819a11e28bfb99eb0343
Kai-46 <kz298@cornell.edu>
2019-11-20 08:40:25 -0500

    add license

--- src/PSL/psl_base/gridVisualize.h
+++ src/PSL/psl_base/gridVisualize.h
@@ -1,19 +1,33 @@
-// This file is part of PlaneSweepLib (PSL)
-
-// Copyright 2016 Christian Haene (ETH Zuerich)
-
-// PSL is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// PSL is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with PSL.  If not, see <http://www.gnu.org/licenses/>.
+// ===============================================================================================================
+// Copyright (c) 2019, Cornell University. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+// the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright otice, this list of conditions and
+//       the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+//       the following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+//     * Neither the name of Cornell University nor the names of its contributors may be used to endorse or
+//       promote products derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+// OF SUCH DAMAGE.
+//
+// Author: Kai Zhang (kz298@cornell.edu)
+//
+// The research is based upon work supported by the Office of the Director of National Intelligence (ODNI),
+// Intelligence Advanced Research Projects Activity (IARPA), via DOI/IBC Contract Number D17PC00287.
+// The U.S. Government is authorized to reproduce and distribute copies of this work for Governmental purposes.
+// ===============================================================================================================
 
 #include "grid.h"
 #include <opencv2/opencv.hpp>
@@ -50,7 +64,6 @@ void saveGridZSliceAsImage(const Grid<T> &grid, const int z, const std::string &
 
   double min, max;
   cv::minMaxLoc(sliceMat, &min, &max);
-//	std::cout << "min: " << min << ", max: " << max << std::endl;
   cv::imwrite(fileName, (sliceMat - min) / (max - min) * 255);
 }
 
--- src/PSL/psl_stereo/cudaPlaneSweep.cpp
+++ src/PSL/psl_stereo/cudaPlaneSweep.cpp
@@ -1,3 +1,35 @@
+// ===============================================================================================================
+// Copyright (c) 2019, Cornell University. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+// the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright otice, this list of conditions and
+//       the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+//       the following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+//     * Neither the name of Cornell University nor the names of its contributors may be used to endorse or
+//       promote products derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+// OF SUCH DAMAGE.
+//
+// Author: Kai Zhang (kz298@cornell.edu)
+//
+// The research is based upon work supported by the Office of the Director of National Intelligence (ODNI),
+// Intelligence Advanced Research Projects Activity (IARPA), via DOI/IBC Contract Number D17PC00287.
+// The U.S. Government is authorized to reproduce and distribute copies of this work for Governmental purposes.
+// ===============================================================================================================
+
+
 // This file is part of PlaneSweepLib (PSL)
 
 // Copyright 2016 Christian Haene (ETH Zuerich)
@@ -358,16 +390,16 @@ void CudaPlaneSweep::process(int refImgId,
     bestPlaneBuffer.download(bestPlanes.getDataPtr());
     bestPlaneCostBuffer.download(bestCosts.getDataPtr());
 
-//    // filter value
-//    if (_filterEnabled) {
-//      for (int i = 0; i < bestPlanes.getWidth(); ++i) {
-//        for (int j = 0; j < bestPlanes.getHeight(); ++j) {
-//          if (bestCosts(i, j) > _filterThreshold) {
-//            bestPlanes(i, j) = absurdPlane;
-//          }
-//        }
-//      }
-//    }
+    // filter value
+    if (_filterEnabled) {
+      for (int i = 0; i < bestPlanes.getWidth(); ++i) {
+        for (int j = 0; j < bestPlanes.getHeight(); ++j) {
+          if (bestCosts(i, j) > _filterThreshold) {
+            bestPlanes(i, j) = absurdPlane;
+          }
+        }
+      }
+    }
 
     // guided filter the label map
     // cv::Mat tmpMat(bestPlanes.getHeight(), bestPlanes.getWidth(), CV_32F, bestPlanes.getDataPtr());
@@ -409,18 +441,9 @@ void CudaPlaneSweep::process(int refImgId,
         }
       }
 
-      // debug
-      // for (int i=0; i < num_planes; ++i) {
-      //   std::cout << "plane " << i << ": ";
-      //   for (int j=0; j < 4; ++j) {
-      //     std::cout << tmp_planes[i * 4 + j] << ",";
-      //   }
-      //   std::cout << std::endl;
-      // }
-
       float absurdXYZ = 1e10f;
       PSL_CUDA::unprojectLabelMap(bestPlaneBuffer, tmp_planes, num_planes, P, absurdXYZ, xMapBuffer, yMapBuffer, zMapBuffer);
-
+      // free memory
       delete [] tmp_planes;
 
       // download to cpu
@@ -435,7 +458,6 @@ void CudaPlaneSweep::process(int refImgId,
         // saveGridZSliceAsImage<float>(xyzMap, 1, _outputFolder + "best/best_y.png");
         // saveGridZSliceAsImage<float>(xyzMap, 2, _outputFolder + "best/best_z.png");
       }
-
 
       if (_outputPointCloudEnabled) {
         cv::Mat refImgGray = downloadImage(refImgId);
@@ -487,10 +509,6 @@ void CudaPlaneSweep::process(int refImgId,
 //
 //      WriteBinaryPlyPoints(plyFilename, plyPoints);
 //    }
-
-    // debug
-    // Eigen::Vector3d point = refImg.cam.unprojectPoint(2079, 2079, planes[87]);
-    // std::cout << "C code: " << point(0) << ", " << point(1) << ", " << point(2) << std::endl;
   }
 
   // deallocate GPU buffer
